7656b62b2ced1b58a094d72379d6de628d8439ac
GhostAvenger <williamseo1234@gmail.com>
2020-10-01 13:19:20 -0700

    Updated

--- app_setup.sh
+++ app_setup.sh
@@ -1,2 +1,12 @@
 #!/bin/bash -x
-ssh todoapp
+echo Copying setup folder to remote host
+scp -r /acit4640/module2/module2/setup todoapp
+echo Moving file to correct location
+ssh todoapp "su - root <<!
+password
+!
+if [ -f /home/admin/todoapp ]; then
+	mv /home/admin/todoapp /home/todoapp
+fi"
+echo Sshing to todoapp and executing install_script.sh
+ssh todoapp "bash /home/todoapp/todoapp/install_script.sh"
---
+++ todoapp/install_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -x
+su - todoapp <<!
+cd /home/todoapp
+echo hello
+!
