f6ea16dae36084ab8f134a444a70548654689ec5
GhostAvenger <williamseo1234@gmail.com>
2020-10-01 18:46:16 -0700

    Beta

--- setup/install_script.sh
+++ setup/install_script.sh
@@ -1,69 +1,68 @@
 #!/bin/bash -x
-sudo su - todoapp -c "
-cd ~;
-echo Login as todoapp successful;
+TODOHOME=/home/todoapp
+APP=/home/todoapp/app
 
+sudo bash -c "
 echo Cloning from github ACIT4640 todo app
-git clone https://github.com/timoguic/ACIT4640-todo-app.git app
+git clone https://github.com/timoguic/ACIT4640-todo-app.git ${APP}
 
 echo Updating database.js
-[ -e file ] && sudo rm /home/todoapp/app/config/database.js
-sudo mv /home/todoapp/setup/database.js /home/todoapp/app/config
+cp ${TODOHOME}/setup/database.js ${APP}/config
 
 echo Installing NodeJS
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
-sudo dnf install nodejs
+dnf install nodejs
 
 echo Installing NodeJS dependencies
-cd /home/todoapp/app
+cd ${APP}
 npm install
 
 echo Checking node status
 node --version
 
 echo Changing Selinux to disabled
-[ -e file ] && sudo rm /etc/selinux/config
-sudo mv /home/todoapp/setup/config /etc/selinux
+cp ${TODOHOME}/setup/config /etc/selinux
 
 echo Installing mongodb
-[ -e file ] && sudo rm /etc/yum.repos.d/mongodb-org-4.4.repo
-sudo mv /home/todoapp/setup/mongodb-org-4.4.repo /etc/yum.repos.d
-sudo dnf install -y mongodb-org
+cp ${TODOHOME}/setup/mongodb-org-4.4.repo /etc/yum.repos.d
+dnf install -y mongodb-org
 
 echo Starting mongodb service
-sudo systemctl enable mongd
-sudo systemctl start mongod
+systemctl enable mongd
+systemctl start mongod
 
 echo Setting up todoapp service
-[ -e file ] && sudo rm /etc/systemd/system/todoapp.service
-sudo mv /home/todoapp/setup/todoapp.service /etc/systemd/system
+cp ${TODOHOME}/setup/todoapp.service /etc/systemd/system
 
 echo Reloading systemctl daemons
-sudo systemctl daemon-reload
+systemctl daemon-reload
 
 echo Starting todoapp service
-sudo systemctl enable todoapp
-sudo systemctl start todoapp
+systemctl enable todoapp
+systemctl start todoapp
 
 echo Installing epel release
-sudo dnf install -y epel-release
+dnf install -y epel-release
 
 echo Installing Nginx
-sudo dnf install -y nginx
+dnf install -y nginx
 
 echo Starting Nginx
-sudo systemctl enable nginx
-sudo systemctl start nginx
+systemctl enable nginx
+systemctl start nginx
 
 echo Changing Firewall
-sudo firewall-cmd --zone=public --add-port=8080/tcp
-sudo firewall-cmd --zone=public --add-port=http
-sudo firewall-cmd --runtime-to-permanenit
+firewall-cmd --zone=public --add-port=8080/tcp
+firewall-cmd --zone=public --add-port=http
+firewall-cmd --runtime-to-permanent
 
 echo Ensuring correct file permissions
-sudo chmod a+rx /home/todoapp
+chmod a+rx ${TODOHOME}
 
-echo Copying ngnix conf to its directory;
-[ -e file ] && sudo rm /etc/nginx/nginx.conf;
-sudo mv /home/todoapp/setup/nginx.conf /etc/nginx/nginx.conf;
+echo Copying ngnix conf to its directory
+cp ${TODOHOME}/setup/nginx.conf /etc/nginx/nginx.conf;
+
+echo Changing perms for /home/todoapp
+chown -R todoapp:todoapp ${TODOHOME}
+chmod 755 ${TODOHOME}
 "
